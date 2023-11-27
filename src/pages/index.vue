9661d4377635865ba6192fa689a0ca1dae47dd80
god-of-js <henryeze019@gmail.com>
2023-11-27 13:46:17 +0100

    :coffin: Unused components deleted
    deleted base components that are not in use

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,30 +1,7 @@
 <script setup lang="ts" generic="T extends any, O extends any">
-import ContactsForm from '~/components/steps/ContactsForm.vue'
-import RegistrationForm from '~/components/steps/RegistrationForm.vue'
-import ProfileInfoForm from '~/components/steps/ProfileInfoForm.vue'
-
 defineOptions({
   name: 'IndexPage',
 })
-
-const steps = {
-  RegistrationForm,
-  ProfileInfoForm,
-  ContactsForm,
-}
-
-type StepKeys = keyof typeof steps
-const stepKeys = Object.keys(steps) as StepKeys[]
-const activeStep = ref<StepKeys>(stepKeys[0])
-
-function goNext() {
-  const indexOfActiveStep = stepKeys.findIndex(step => activeStep.value === step)
-
-  if (indexOfActiveStep < stepKeys.length - 1)
-    activeStep.value = stepKeys[indexOfActiveStep + 1]
-  // eslint-disable-next-line no-alert
-  else alert('Forms completed') // Ignore the alert and the disabled eslint rule. I need it for only this line.
-}
 </script>
 
 <template>
@@ -33,14 +10,6 @@ function goNext() {
       <div i-carbon-campsite text-xl />
       <span>PROJECT X</span>
     </div>
-    <div class="mx-auto lg:w-50% md:w-70%">
-      <TheStepper :steps="stepKeys" :active-step="activeStep" class="mx-auto md:mx-0" />
-
-      <div class="my-20">
-        <KeepAlive>
-          <Component :is="steps[activeStep]" @go-to-next="goNext" />
-        </KeepAlive>
-      </div>
-    </div>
+    <div class="mx-auto lg:w-50% md:w-70%" />
   </div>
 </template>
