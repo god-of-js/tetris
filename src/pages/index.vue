12abb81f0651e52baef5adb0f122a04c0f9e014c
god-of-js <henryeze019@gmail.com>
2023-11-28 11:24:10 +0100

    :sparkles: Drag and drop
    drag and drop implemented using drag and drop api

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,15 +1,67 @@
 <script setup lang="ts" generic="T extends any, O extends any">
+import TheButton from '~/components/TheButton.vue'
+
 defineOptions({
   name: 'IndexPage',
 })
+
+const shapes = ref(['triangle', 'rectangle', 'square'])
+
+function onDrop(event: DragEvent) {
+  const shape = event.dataTransfer?.getData('shape')
+  alert(shape)
+}
+
+function startDrag(event: DragEvent, shape: string) {
+  if (!event.dataTransfer)
+    return
+  event.dataTransfer.dropEffect = 'move'
+  event.dataTransfer.effectAllowed = 'move'
+  event.dataTransfer.setData('shape', shape)
+}
 </script>
 
 <template>
   <div class="mx-auto mb-20 md:w-65%">
-    <div class="flex items-center gap-2 md:fixed">
+    <div class="mb-8 flex items-center gap-2">
       <div i-carbon-campsite text-xl />
-      <span>PROJECT X</span>
+      <span>Tetris</span>
     </div>
-    <div class="mx-auto lg:w-50% md:w-70%" />
+    <div class="mx-auto flex flex-col gap-8 md:flex-row">
+      <div class="grid h-fit gap-4 align-start">
+        <TheButton class="h-fit">
+          Clear
+        </TheButton>
+        <div v-for="shape in shapes" :key="shape" :class="shape" draggable="true" @dragstart="startDrag($event, shape)" />
+      </div>
+      <div id="board" class="board flex items-end justify-center bg-gray-50" @drop="onDrop" @dragover.prevent @dragenter.prevent>
+        <div class="triangle" />
+      </div>
+    </div>
   </div>
 </template>
+
+<style>
+.triangle {
+  border-bottom: 30px solid blue;
+  border-right: 30px solid transparent;
+  width: 0;
+  height: 0;
+}
+
+.rectangle {
+  height: 60px;
+  width: 30px;
+  background: red;
+}
+
+.square {
+  width: 30px;
+  height: 30px;
+  background: yellow;
+}
+.board {
+  width: 600px;
+  height: 400px;
+}
+</style>
